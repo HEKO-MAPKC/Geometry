54106c2cadd92daabc7b3c78602dfc5dc26f153b
Michael <michaelkim@ya.ru>
2020-05-28 20:00:21 +0700

    Create multi file application

---
+++ src/func.h
@@ -0,0 +1,24 @@
+
+#ifndef PERIMETER_H
+#define PERIMETER_H
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+float perimeters(float r);
+
+float areas(float r);
+
+void out_figure();
+
+int proverka(string com);
+
+int get_num(string com, int pos, int start_pos);
+
+void get_figure();
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "func.h"
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+const double PI = 3.1415926535;
+static const size_t npos = -1;
+
+using namespace std;
+
+int main()
+{
+    get_figure();
+    out_figure();
+    system("PAUSE");
+}
